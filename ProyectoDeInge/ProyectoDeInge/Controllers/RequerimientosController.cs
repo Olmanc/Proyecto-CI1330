6a4ab5c8c71fff524d502cde3b2630b29d1164a6
BryanSibaja <bryan.sibaja@ucr.ac.cr>
2016-10-21 09:33:36 -0600

    Requerimientos

---
+++ ProyectoDeInge/ProyectoDeInge/Controllers/RequerimientosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoDeInge.Models;
+
+namespace ProyectoDeInge.Controllers
+{
+    public class RequerimientosController : Controller
+    {
+        private BD_IngeGrupo2Entities2 db = new BD_IngeGrupo2Entities2();
+
+        // GET: Requerimientos
+        public ActionResult Index()
+        {
+            var rEQUERIMIENTOS = db.REQUERIMIENTOS.Include(r => r.PROYECTO).Include(r => r.USUARIOS);
+            return View(rEQUERIMIENTOS.ToList());
+        }
+
+        // GET: Requerimientos/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            REQUERIMIENTOS rEQUERIMIENTOS = db.REQUERIMIENTOS.Find(id);
+            if (rEQUERIMIENTOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rEQUERIMIENTOS);
+        }
+
+        // GET: Requerimientos/Create
+        public ActionResult Create()
+        {
+            ViewBag.PRYCTOID = new SelectList(db.PROYECTO, "ID", "NOMBRE");
+            ViewBag.ENCARGADO = new SelectList(db.USUARIOS, "CEDULA", "NOMBRE");
+            return View();
+        }
+
+        // POST: Requerimientos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,NOMBRE,ESFUERZO,IMAGEN,DESCRIPCION,PRIORIDAD,OBSERVACIONES,MODULO,FECHAINCIO,FECHAFINAL,ESTADO,ENCARGADO,PRYCTOID,VERSION_ID")] REQUERIMIENTOS rEQUERIMIENTOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.REQUERIMIENTOS.Add(rEQUERIMIENTOS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PRYCTOID = new SelectList(db.PROYECTO, "ID", "NOMBRE", rEQUERIMIENTOS.PRYCTOID);
+            ViewBag.ENCARGADO = new SelectList(db.USUARIOS, "CEDULA", "NOMBRE", rEQUERIMIENTOS.ENCARGADO);
+            return View(rEQUERIMIENTOS);
+        }
+
+        // GET: Requerimientos/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            REQUERIMIENTOS rEQUERIMIENTOS = db.REQUERIMIENTOS.Find(id);
+            if (rEQUERIMIENTOS == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PRYCTOID = new SelectList(db.PROYECTO, "ID", "NOMBRE", rEQUERIMIENTOS.PRYCTOID);
+            ViewBag.ENCARGADO = new SelectList(db.USUARIOS, "CEDULA", "NOMBRE", rEQUERIMIENTOS.ENCARGADO);
+            return View(rEQUERIMIENTOS);
+        }
+
+        // POST: Requerimientos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,NOMBRE,ESFUERZO,IMAGEN,DESCRIPCION,PRIORIDAD,OBSERVACIONES,MODULO,FECHAINCIO,FECHAFINAL,ESTADO,ENCARGADO,PRYCTOID,VERSION_ID")] REQUERIMIENTOS rEQUERIMIENTOS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rEQUERIMIENTOS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PRYCTOID = new SelectList(db.PROYECTO, "ID", "NOMBRE", rEQUERIMIENTOS.PRYCTOID);
+            ViewBag.ENCARGADO = new SelectList(db.USUARIOS, "CEDULA", "NOMBRE", rEQUERIMIENTOS.ENCARGADO);
+            return View(rEQUERIMIENTOS);
+        }
+
+        // GET: Requerimientos/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            REQUERIMIENTOS rEQUERIMIENTOS = db.REQUERIMIENTOS.Find(id);
+            if (rEQUERIMIENTOS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rEQUERIMIENTOS);
+        }
+
+        // POST: Requerimientos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            REQUERIMIENTOS rEQUERIMIENTOS = db.REQUERIMIENTOS.Find(id);
+            db.REQUERIMIENTOS.Remove(rEQUERIMIENTOS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
