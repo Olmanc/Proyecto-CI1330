538285ddd31ae88b6641c54ab2307a28dc296ede
Olman Cheng Lam <b31953@ecci.ucr.ac.cr>
2016-10-26 11:55:45 -0600

    agrega ProyectosViewmodel

---
+++ ProyectoDeInge/ProyectoDeInge/Controllers/ProyectosViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.Web.Mvc;
+
+namespace ProyectoDeInge.Models
+{
+    public class ProyectosViewModel
+    {
+        public PROYECTO modeloProyecto { get; set; }
+        public List<string> listaRecursos = new List<string>();
+        public List<string> recursosDisponibles = new List<string>();
+
+        //public List<string> listaRequerimientos = new List<string>();
+
+        public List<PROYECTO> listaProyectos = new List<PROYECTO>();        
+
+        public IEnumerable<SelectListItem> recursosAsignados { get; set; }
+        public IEnumerable<SelectListItem> otrosRecursos { get; set; }
+        
+
+        public HashSet<string> verificaPermisos = new HashSet<string>();        //para verificar los permisos del usuario actual
+    }
+}
