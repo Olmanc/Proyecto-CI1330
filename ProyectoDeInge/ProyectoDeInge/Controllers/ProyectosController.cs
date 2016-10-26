be8f10a2b89b1d1a461ca9a740415b46352a276e
Olman Cheng Lam <b31953@ecci.ucr.ac.cr>
2016-10-26 11:19:24 -0600

    Vista unificada de proyectos

--- ProyectoDeInge/ProyectoDeInge/Controllers/ProyectosController.cs
+++ ProyectoDeInge/ProyectoDeInge/Controllers/ProyectosController.cs
@@ -17,7 +17,9 @@ namespace ProyectoDeInge.Controllers
         // GET: Proyectos
         public ActionResult Index()
         {
-            return View(db.PROYECTO.ToList());
+            ProyectosViewModel modelo = new ProyectosViewModel();
+            modelo.listaProyectos = db.PROYECTO.ToList();
+            return View(modelo);
         }
 
         // GET: Proyectos/Details/5
@@ -123,6 +125,93 @@ namespace ProyectoDeInge.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public List<string> buscaResursos() {
+            return null;
+        }
+
+        public HashSet<string> obtienePermisos() {
+            var fg = new AspNetUsers();                 //instancia AspNetUser para usuario actual
+            HashSet<string> permisos = new HashSet<string>();
+            var listauser = db.AspNetUsers.ToArray();
+
+            for (int i = 0; i < listauser.Length; i++)
+            {                           //de todos los AspNetUser del sistema, encuentra el que tenga el email activo actualmente
+                if (listauser[i].Email == User.Identity.Name)
+                {
+                    fg = listauser[i];                  //obtiene el AspNetUser actual
+                }
+            }
+
+            AspNetRoles role = fg.AspNetRoles.First();  //consigue el rol del usuario
+            var per = role.PERMISOS;                    //copia los permisos que tiene asignado
+
+            foreach (PERMISOS p in role.PERMISOS)
+            {     //los copia a un HashSet<string>
+                permisos.Add(p.ID);
+            }
+            return permisos;
+        }
+
+        public ActionResult Unificado(string id) {
+            if (id == null) {//si id es null
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProyectosViewModel proyecto = new ProyectosViewModel();//nuevo viewModel
+            proyecto.modeloProyecto = db.PROYECTO.Find(id);//encuentra al proyecto
+            if (proyecto.modeloProyecto == null) {//si el proyecto no existe
+                return HttpNotFound();
+            }
+
+            List<USUARIOS> todosUsuarios = db.USUARIOS.ToList();//todos los usuarios
+            List<USUARIOS> recursos = new List<USUARIOS>();
+            List<USUARIOS> disponibles = new List<USUARIOS>();
+            AspNetRoles rol = db.AspNetRoles.Find("2");//busco rol de desarrollador
+
+            foreach (var item in todosUsuarios) {
+                AspNetUsers asp = db.AspNetUsers.Find(item.ID_ASP);//busca AspNetUser del usuario
+
+                if (asp.AspNetRoles.Contains(rol)) {
+                    if (item.PRYCTOID == id)//si esta en el proyecto lo agrega a recursos
+                    {
+                        recursos.Add(item);
+                    }
+                    else if(item.PRYCTOID == null)//si no esta trabajando en ningun proyecto, lo agrego a recursos disponibles
+                    {
+                        disponibles.Add(item);
+                    }
+                }                
+            }
+            //llena SelectList de recursos asignados
+            proyecto.recursosAsignados = recursos.Select(o => new SelectListItem {
+                Text = o.NOMBRE + " " + o.APELLIDO1 + " " + o.APELLIDO2,
+                Value = o.CEDULA.ToString()
+            });
+            //llena SelectList de recursos disponibles
+            proyecto.otrosRecursos = disponibles.Select(o => new SelectListItem
+            {
+                Text = o.NOMBRE + " " + o.APELLIDO1 + " " + o.APELLIDO2,
+                Value = o.CEDULA.ToString()
+            });
+            
+            proyecto.verificaPermisos = obtienePermisos();
+            
+            return View(proyecto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Unificado(ProyectosViewModel modelo) {
+            if (ModelState.IsValid)
+            {
+                db.Entry(modelo.modeloProyecto).State = EntityState.Modified;
+                var id = modelo.modeloProyecto.ID;
+                /*asignar y desasignar recursos*/
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(modelo);
         }
     }
 }
