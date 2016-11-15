e62601438b5ca8483f9f6b57294b962cc2913e65
munozviolin <yohelmr18@gmail.com>
2016-11-15 11:06:06 -0600

    Versión Correcta Martes 15/11

--- ProyectoDeInge/ProyectoDeInge/Models/REQUERIMIENTOS.cs
+++ ProyectoDeInge/ProyectoDeInge/Models/REQUERIMIENTOS.cs
@@ -12,15 +12,17 @@ namespace ProyectoDeInge.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+
     public partial class REQUERIMIENTOS
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public REQUERIMIENTOS()
         {
             this.CAMBIOS = new HashSet<CAMBIOS>();
+            this.CAMBIOS1 = new HashSet<CAMBIOS>();
             this.CRIT_ACEPTACION = new HashSet<CRIT_ACEPTACION>();
         }
-
+    
         public string ID { get; set; }
         public string NOMBRE { get; set; }
         public Nullable<int> ESFUERZO { get; set; }
@@ -37,15 +39,18 @@ namespace ProyectoDeInge.Models
         public string ENCARGADO { get; set; }
         public string PRYCTOID { get; set; }
         public int VERSION_ID { get; set; }
-
+        public string ESTADO_CAMBIOS { get; set; }
+        public Nullable<int> SPRINT { get; set; }
+    
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<CAMBIOS> CAMBIOS { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<CAMBIOS> CAMBIOS1 { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<CRIT_ACEPTACION> CRIT_ACEPTACION { get; set; }
         public virtual PROYECTO PROYECTO { get; set; }
-
-        public HashSet<string> verificaPermisos = new HashSet<string>();
         public virtual USUARIOS USUARIOS { get; set; }
+        public HashSet<string> verificaPermisos = new HashSet<string>();
         internal void crearCriterios(int c = 1)
         {
             for (int i = 0; i < c; i++)
