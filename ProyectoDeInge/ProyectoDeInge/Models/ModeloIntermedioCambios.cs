7f7d210c2c6bf1344baa2d51ac3a85fed93f0595
JaviAM20 <isurus.javi@gmail.com>
2016-11-23 09:17:23 -0600

    Requerimientos (No estaba por alguna razon)

---
+++ ProyectoDeInge/ProyectoDeInge/Models/ModeloIntermedioCambios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoDeInge.Models
+{
+    public class ModeloIntermedioCambios
+    {
+       public REQUERIMIENTOS actual;
+       public REQUERIMIENTOS consultado;
+    }
+}
